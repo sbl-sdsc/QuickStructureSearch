f9592bdee34c1523a4aef4b7195acc4dd8877ba4
Peter Rose <pwrose@ucsd.edu>
2015-06-24 14:22:32 -0700

    Added RandomFragmentCreator and RandomFragmentMapper classes

---
+++ src/main/java/org/rcsb/project1/RandomFragmentCreator.java
@@ -0,0 +1,139 @@
+package org.rcsb.project1;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+import javax.vecmath.Point3d;
+
+import org.apache.hadoop.io.ArrayWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
+import org.rcsb.structuralSimilarity.ChainPairLengthFilter;
+import org.rcsb.structuralSimilarity.ChainPairToTmMapper;
+import org.rcsb.structuralSimilarity.GapFilter;
+import org.rcsb.structuralSimilarity.LengthFilter;
+import org.rcsb.structuralSimilarity.SeqToChainMapper;
+
+import scala.Tuple2;
+
+/**
+ * This class creates structural alignments between random protein chain pairs 
+ * using jFatCAT and scores the alignments with the TM score
+ * 
+ * @author  Peter Rose
+ */
+public class RandomFragmentCreator { 
+	private static int NUM_THREADS = 8;
+	private static int NUM_TASKS_PER_THREAD = 3; // Spark recommends 2-3 tasks per thread
+	private static int BATCH_SIZE = 1000;
+
+	public static void main(String[] args ) throws FileNotFoundException
+	{
+		String sequenceFileName = args[0]; 
+		String outputFileName = args[1];
+		int nPairs = Integer.parseInt(args[2]);
+		int seed = Integer.parseInt(args[3]);
+		
+		long t1 = System.nanoTime();
+		RandomFragmentCreator creator = new RandomFragmentCreator();
+		creator.run(sequenceFileName, outputFileName, nPairs, seed);
+		System.out.println("Time: " + ((System.nanoTime()-t1)/1E9) + " s");
+	}
+
+	private void run(String path, String outputFileName, int nPairs, int seed) throws FileNotFoundException {
+		// setup spark
+		SparkConf conf = new SparkConf()
+				.setMaster("local[" + NUM_THREADS + "]")
+				.setAppName("1" + this.getClass().getSimpleName())
+				.set("spark.driver.maxResultSize", "2g");
+//				.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		
+		// Step 1. calculate <pdbId.chainId, feature vector> pairs
+        List<Tuple2<String, Point3d[]>> chains = sc
+				.sequenceFile(path, Text.class, ArrayWritable.class, NUM_THREADS)  // read protein chains
+			//	.sample(false, 0.1, 123456) // use only a random fraction, i.e., 10%
+				.mapToPair(new SeqToChainMapper()) // convert input to <pdbId.chainId, CA coordinate> pairs
+				.filter(new GapFilter(0, 0)) // keep protein chains with gap size <= 0 and 0 gaps
+				.filter(new LengthFilter(50,500)) // keep protein chains with 50 - 500 residues
+				.collect(); // return results to master node
+
+		// Step 2.  broadcast feature vectors to all nodes
+		final Broadcast<List<Tuple2<String,Point3d[]>>> chainsBc = sc.broadcast(chains);
+		int nChains = chains.size();
+
+		Random r = new Random(seed);
+
+		PrintWriter writer = new PrintWriter(outputFileName);
+		
+		for (int i = 0; i < nPairs; i+=BATCH_SIZE) {
+			List<Tuple2<Integer,Integer>> pairs = randomPairs(nChains, BATCH_SIZE, r.nextLong());
+
+			List<Tuple2<String, Double[]>> list = sc
+					.parallelizePairs(pairs, NUM_THREADS*NUM_TASKS_PER_THREAD) // distribute data
+					.mapToPair(new RandomFragmentMapper(chainsBc)) // maps pairs of chain id indices to chain id, TM score pairs
+					//				.filter(s -> s._2 > 0.9f) //
+					.collect();	// copy result to master node
+
+			// write results to .csv file
+			writeToCsv(writer, list);
+		}
+
+		writer.close();
+		sc.stop();
+		sc.close();
+
+		System.out.println("protein chains     : " + nChains);
+		System.out.println("ramdom pairs        : " + nPairs);
+	}
+
+	/**
+	 * Writes pairs of chain ids and calculated similarity score to a csv file
+	 * @param writer
+	 * @param list
+	 */
+	private static void writeToCsv(PrintWriter writer, List<Tuple2<String, Double[]>> list) {
+		for (Tuple2<String, Double[]> t : list) {
+			writer.print(t._1);
+			for (Double f: t._2) {
+				writer.print(",");
+			    writer.print(f);
+			}
+			writer.println();
+		}
+		writer.flush();
+	}
+
+	/**
+	 * Returns random pairs of indices for the pairwise comparison.
+	 * @param n number of feature vectors
+	 * @return
+	 */
+	private List<Tuple2<Integer, Integer>> randomPairs(int n, int nPairs, long seed) {
+		Random r = new Random(seed);
+		Set<Tuple2<Integer,Integer>> set = new HashSet<>(nPairs);
+
+		for (int i = 0; i < nPairs; i++) {
+			int j = r.nextInt(n);
+			int k = r.nextInt(n);
+			if (j == k) {
+				continue;
+			}
+
+			Tuple2<Integer,Integer> tuple = new Tuple2<>(j,k);
+			if (! set.contains(tuple)) {
+			    set.add(tuple);
+			}
+		}
+		return new ArrayList<Tuple2<Integer,Integer>>(set);
+	}
+}
+
---
+++ src/main/java/org/rcsb/project1/RandomFragmentMapper.java
@@ -0,0 +1,67 @@
+package org.rcsb.project1;
+
+import java.util.List;
+import java.util.Random;
+
+import javax.vecmath.Point3d;
+
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.broadcast.Broadcast;
+import org.apache.spark.mllib.linalg.Vector;
+import org.rcsb.structuralSimilarity.TmScorer;
+
+import scala.Tuple2;
+
+/**
+ * This class maps a pair of chains, specified by two indices into the broadcasted data
+ * to a vector of alignment scores
+ * 
+ * @author  Peter Rose
+ */
+public class RandomFragmentMapper implements PairFunction<Tuple2<Integer,Integer>,String,Double[]> {
+	private static final long serialVersionUID = 1L;
+	private Broadcast<List<Tuple2<String, Point3d[]>>> data = null;
+
+
+	public RandomFragmentMapper(Broadcast<List<Tuple2<String,Point3d[]>>> data) {
+		this.data = data;
+	}
+
+	/**
+	 * Returns a chainId pair with the TM scores
+	 */
+	public Tuple2<String, Double[]> call(Tuple2<Integer, Integer> tuple) throws Exception {
+		Tuple2<String,Point3d[]> t1 = this.data.getValue().get(tuple._1);
+		Tuple2<String,Point3d[]> t2 = this.data.getValue().get(tuple._2);
+		
+		
+		
+		Point3d[] points1 = t1._2;
+		Point3d[] points2 = t2._2;
+		
+		int len1 = points1.length - 8;
+		int len2 = points2.length - 8;
+		Random r = new Random();
+		
+		int start1 = r.nextInt(len1);
+		int start2 = r.nextInt(len2);
+		
+		StringBuilder key = new StringBuilder();
+		key.append(t1._1);
+		key.append(",");
+		key.append(t2._1);
+		key.append(",");
+		key.append(start1);
+		key.append(",");
+		key.append(start2);
+
+		Double[] rmsds = new Double[2];
+		
+        // rmsds[0] = ... crmsd
+		// rmsds[1] = ... drmsd
+		
+		System.out.println(key);
+		
+		return new Tuple2<String, Double[]>(key.toString(), rmsds);
+    }
+}
