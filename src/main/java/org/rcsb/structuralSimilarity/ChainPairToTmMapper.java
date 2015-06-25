cfe5fd0abf4949e3a0b8fd7ba60a70870196bcb3
roaderj <chrisli615@gmail.com>
2015-06-25 14:28:10 -0700

    Quick fix

--- src/main/java/org/rcsb/structuralSimilarity/ChainPairToTmMapper.java
+++ src/main/java/org/rcsb/structuralSimilarity/ChainPairToTmMapper.java
@@ -6,7 +6,6 @@ import javax.vecmath.Point3d;
 
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.broadcast.Broadcast;
-import org.apache.spark.mllib.linalg.Vector;
 
 import scala.Tuple2;
 
@@ -28,7 +27,7 @@ public class ChainPairToTmMapper implements PairFunction<Tuple2<Integer,Integer>
 	/**
 	 * Returns a chainId pair with the TM scores
 	 */
-	public Tuple2<String, Double[]> call(Tuple2<Integer, Integer> tuple) throws Exception {
+	public Tuple2<String, Float[]> call(Tuple2<Integer, Integer> tuple) throws Exception {
 		Tuple2<String,Point3d[]> t1 = this.data.getValue().get(tuple._1);
 		Tuple2<String,Point3d[]> t2 = this.data.getValue().get(tuple._2);
 		
@@ -40,30 +39,8 @@ public class ChainPairToTmMapper implements PairFunction<Tuple2<Integer,Integer>
 		Point3d[] points1 = t1._2;
 		Point3d[] points2 = t2._2;
 
-		int len1 = points1.length - 8;
-		int len2 = points2.length - 8;
-		Random r = new Random();
+		Float[] scores = TmScorer.getFatCatTmScore(points1, points2);
 		
-		int start1 = r.nextInt(len1);
-		int start2 = r.nextInt(len2);
-		
-		StringBuilder key = new StringBuilder();
-		key.append(t1._1);
-		key.append(",");
-		key.append(t2._1);
-		key.append(",");
-		key.append(start1);
-		key.append(",");
-		key.append(start2);
-		
-		Double[] rmsds = new Double[2];
-		
-		// rmsds[0] =  . . crmsd
-		// rmsds[1] =  . . drmsd
-		
-		System.out.println(key);
-		
-		return new Tuple2<String, Double[](key.toString(), rmsds)>;
-		
+        return new Tuple2<String, Float[]>(key.toString(), scores);
     }
 }
