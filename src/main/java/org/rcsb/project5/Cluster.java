42c1c410b76068fd14547b03e4ada664e2211cb6
EatChow513 <joesun.513@gmail.com>
2015-08-05 14:00:15 -0700

    Added method for finding structurally representative chains and method for merging clusters. Also added javadoc comments

--- src/main/java/org/rcsb/project5/Cluster.java
+++ src/main/java/org/rcsb/project5/Cluster.java
@@ -10,6 +10,7 @@ import org.rcsb.structuralAlignment.SuperPositionQCP;
 import org.rcsb.structuralSimilarity.TmScorer;
 
 import scala.Tuple2;
+import scala.Tuple3;
 
 /**
  * This class represents a cluster of protein chains with sequence similarity and structural similarity. 
@@ -27,15 +28,17 @@ public class Cluster {
 	private int strClusterId; // Structural Cluster Id
 	private List<Tuple2<String, SimplePolymerChain>> strCluster;
 	private Tuple2<String, SimplePolymerChain> repChain; // representative chain
+	private List<Tuple3<String, SimplePolymerChain, Double>> strRepChain;
 	private SuperPositionQCP qcp;
 	private LongestCommonSubstring lcs;
 	private double gapPen; // penalty for each gap in the Point3d array
 	private double holePen; // penalty for each hole in the Point3d array
-	
+
 	public Cluster(int seqClusterId, int strClusterId, List<Tuple2<String, SimplePolymerChain>> strCluster) {
 		this.seqClusterId = seqClusterId;
 		this.strClusterId = strClusterId;
 		this.strCluster = strCluster;
+		strRepChain = new ArrayList<Tuple3<String, SimplePolymerChain, Double>>();
 	}
 
 	public Cluster(int seqClusterId, int strClusterId, List<Tuple2<String, SimplePolymerChain>> strCluster, Tuple2<String, SimplePolymerChain> repChain, double gapPen, double holePen) {
@@ -47,11 +50,24 @@ public class Cluster {
 		this.holePen = holePen;
 		qcp = new SuperPositionQCP();
 		lcs = new LongestCommonSubstring();
+		strRepChain = new ArrayList<Tuple3<String, SimplePolymerChain, Double>>();
+	}
+	
+	public Cluster(int seqClusterId, int strClusterId, List<Tuple2<String, SimplePolymerChain>> strCluster, Tuple2<String, SimplePolymerChain> repChain, double gapPen, double holePen, List<Tuple3<String, SimplePolymerChain, Double>> strRepChains) {
+		this.seqClusterId = seqClusterId;
+		this.strClusterId = strClusterId;
+		this.strCluster = strCluster;
+		this.repChain = repChain;
+		this.gapPen = gapPen;
+		this.holePen = holePen;
+		qcp = new SuperPositionQCP();
+		lcs = new LongestCommonSubstring();
+		strRepChain = strRepChains;
 	}
 
 	/**
 	 * returns the Sequence Cluster Id of the cluster
-	 * @return seqClusterId
+	 * @return seqClusterId the sequence cluster Id of the cluster
 	 */
 	public int getSeqClusterId() {
 		return seqClusterId;
@@ -59,15 +75,31 @@ public class Cluster {
 
 	/**
 	 * returns the Structural Cluster Id of the cluster
-	 * @return strClusterId
+	 * @return strClusterId the structural cluster Id of the cluster
 	 */
 	public int getStrClusterId() {
 		return strClusterId;
 	}
+	
+	/**
+	 * sets the sequence cluster Id of the cluster to a given number
+	 * @param seqClusterId the sequence cluster Id of the cluster
+	 */
+	public void setSeqClusterId(int seqClusterId) {
+		this.seqClusterId = seqClusterId;
+	}
+	
+	/**
+	 * sets the structural cluster Id of the cluster to a given number
+	 * @param strClusterId the structural cluster Id of the cluster
+	 */
+	public void setStrClusterId(int strClusterId) {
+		this.strClusterId = strClusterId;
+	}
 
 	/**
 	 * returns the entire structural cluster as a List of Tuple2s
-	 * @return strCluster
+	 * @return strCluster the structural cluster as a List<Tuple2<String, SimplePolymerChain>>
 	 */
 	public List<Tuple2<String, SimplePolymerChain>> getStrCluster() {
 		return strCluster;
@@ -75,10 +107,14 @@ public class Cluster {
 
 	/**
 	 * returns the representative chain of the structural cluster
-	 * @return repChain
+	 * @return repChain the representative chain of the structural cluster
 	 */
 	public Tuple2<String, SimplePolymerChain> getRepChain() {
 		return repChain;
+	}
+
+	public List<Tuple3<String, SimplePolymerChain, Double>> getStrRepChain() {
+		return strRepChain;
 	}
 
 	/**
@@ -91,17 +127,24 @@ public class Cluster {
 
 	/**
 	 * sets the representative chain of the cluster as the chain given in the argument
-	 * @param tuple
+	 * @param tuple the desired representative chain of the cluster
 	 */
 	public void setRepChain(Tuple2<String, SimplePolymerChain> tuple) {
 		repChain = tuple;
 	}
 
 	/**
+	 * adds a structural representative chain to the list of structural representative chains for the cluster
+	 * @param tuple the structural representative chain to be added
+	 */
+	public void addStrRepChain(Tuple3<String, SimplePolymerChain, Double> tuple) {
+		strRepChain.add(tuple);
+	}
+
+	/**
 	 * Finds the representative chain of the structural cluster by using a scoring method which 
 	 * adds scores for chain length and subtracts scores for gaps and holes in the chain. The 
-	 * chain with the highest score is set as the representative chain. If there are multiple 
-	 * chains tied with the highest score, tiebreaker() is called.
+	 * chain with the highest score is set as the representative chain. 
 	 */
 	public void findRepChain() {
 		if(isNull()) {
@@ -112,7 +155,7 @@ public class Cluster {
 		int gaps;
 		int holes;
 		for(int n = 0; n < size(); n ++) {
-/*			List<Tuple2<String, SimplePolymerChain>> a = getStrCluster();
+			/*			List<Tuple2<String, SimplePolymerChain>> a = getStrCluster();
 			Tuple2<String, SimplePolymerChain> b = a.get(n);
 			SimplePolymerChain c = b._2;
 			if(c == null) {
@@ -122,7 +165,7 @@ public class Cluster {
 			}
 			Point3d[] d = c.getCoordinates();
 			double e = d.length;*/
-/*			System.out.println(this);
+			/*			System.out.println(this);
 			System.out.println(this.getStrCluster().get(n)._1);*/
 			tempScore = getStrCluster().get(n)._2.getCoordinates().length;
 			Point3d[] coordinates = strCluster.get(n)._2.getCoordinates();
@@ -131,15 +174,12 @@ public class Cluster {
 			boolean newGap = true;
 			for(int index = 0; index < coordinates.length; index ++) {
 				if(coordinates[index] == null) {
+					holes ++;
 					if(newGap)
 						gaps ++;
 					newGap = false;
 				} else
 					newGap = true;
-			}
-			for(int pos = 0; pos < coordinates.length; pos ++) {
-				if(coordinates[pos] == null)
-					holes ++;
 			}
 			tempScore = tempScore - gaps*gapPen - holes*holePen;
 			scores[n] = tempScore;
@@ -210,21 +250,106 @@ public class Cluster {
 		}
 		setRepChain(tiebreakerList.get(minIndex));*/
 	}
-	
+
+	/**
+	 * finds the structural representative chain of a cluster by finding the chain in the cluster which
+	 * has the least value of the sums of the RMSD values of that chain and all other chains in the
+	 * cluster
+	 */
+	public void findStrRepChain() {
+		double[][] array = new double[strCluster.size()][strCluster.size()];
+		List<Integer> startEnd = null;
+		double cRmsd;
+		for (int outer = 0; outer < array.length - 1; outer++) {
+			for (int inner = outer + 1; inner < array.length; inner++) {
+				startEnd = lcs.longestCommonSubstring(
+						strCluster.get(outer)._2.getSequence(), strCluster.get(inner)._2.getSequence());
+				cRmsd = getcRmsd(strCluster.get(outer), strCluster.get(inner), startEnd.get(0),
+						startEnd.get(1), startEnd.get(2), startEnd.get(3));
+				array[outer][inner] = cRmsd;
+			}
+		}
+		
+		double[] simValue = new double[strCluster.size()];
+		for (int outer = 0; outer < simValue.length; outer++) {
+			for (int inner = 0; inner < simValue.length; inner++) {
+				if (inner > outer) {
+					if(simValue[outer] < array[outer][inner]) {
+						simValue[outer] = array[outer][inner];
+					}
+				} else if (inner < outer) {
+					if(simValue[outer] < array[inner][outer]) {
+						simValue[outer] = array[inner][outer];
+					}
+				}
+			}
+		}
+
+		double min = simValue[0];
+		int minIndex = 0;
+		for (int i = 1; i < simValue.length; i++) {
+			if(simValue[i] < min) {
+				min = simValue[i];
+				minIndex = i;
+			}
+		}
+		strRepChain.add(new Tuple3<String, SimplePolymerChain, Double>(strCluster.get(minIndex)._1, strCluster.get(minIndex)._2, simValue[minIndex]));
+	}
+
+	public Cluster merge(Cluster c, int seqClusterId, int strClusterId) {
+		double[] scores = new double[2];
+		double tempScore;
+		Point3d[] coordinates;
+		int gaps;
+		int holes;
+		for(int n = 0; n < 2; n ++) {
+			if(n == 0) {
+				tempScore = this.getRepChain()._2.getCoordinates().length;
+				coordinates = this.getRepChain()._2.getCoordinates();
+			} else {
+				tempScore = c.getRepChain()._2.getCoordinates().length;
+				coordinates = c.getRepChain()._2.getCoordinates();
+			}
+			gaps = 0;
+			holes = 0;
+			boolean newGap = true;
+			for(int index = 0; index < coordinates.length; index ++) {
+				if(coordinates[index] == null) {
+					holes ++;
+					if(newGap)
+						gaps ++;
+					newGap = false;
+				} else
+					newGap = true;
+			}
+			tempScore = tempScore - gaps*gapPen - holes*holePen;
+			scores[n] = tempScore;
+		}
+		Tuple2<String, SimplePolymerChain> newRepChain;
+		if(scores[0] >= scores[1]) {
+			newRepChain = this.getRepChain();
+		} else {
+			newRepChain = c.getRepChain();
+		}
+		strCluster.addAll(c.getStrCluster());
+		strRepChain.addAll(c.getStrRepChain());
+		return new Cluster(seqClusterId, strClusterId, strCluster, newRepChain, this.gapPen, this.holePen, strRepChain);
+	}
+
 	public double similarity() {
 		if(isNull()) {
 			return -1;
 		}else if(size() <= 1) {
 			return 0;
 		}
-		
+
 		if(getRepChain() == null) {
 			findRepChain();
 		}
-		
+
 		double sum = 0;
 		List<Integer> startEnd = null;
-		
+
 		for(int i = 0; i < size(); i++) {
 			startEnd = lcs.longestCommonSubstring(
 					getRepChain()._2.getSequence(), strCluster.get(i)._2.getSequence());
@@ -233,15 +358,15 @@ public class Cluster {
 		}
 		return sum / (size() - 1);
 	}
-	
+
 	public double compare(Cluster c) {
 		if(isNull() || c.isNull()) {
 			return -1;
 		}
-		
+
 		double max = 0;
 		double temp;
-		
+
 		for(Tuple2<String, SimplePolymerChain> tuple1: strCluster) {
 			for(Tuple2<String, SimplePolymerChain> tuple2: c.getStrCluster()) {
 				temp = TmScorer.getFatCatTmScore(tuple1._2.getCoordinates(), tuple2._2.getCoordinates())[1];
@@ -250,10 +375,10 @@ public class Cluster {
 				}
 			}
 		}
-		
+
 		return max;
 	}
-	
+
 	/**
 	 * returns a boolean which identifies whether or not the cluster is null
 	 * @return true or false
