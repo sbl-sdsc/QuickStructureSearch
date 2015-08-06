b5abe078d0078ce638d75eb08f5b2a8f0aa8b558
Grant Summers <grant.samuel.summers@gmail.com>
2015-08-06 16:15:43 -0700

    Removed faulty triangle inequality, added frequency counter to library, removed comments (they were getting out of hand)

--- src/main/java/org/rcsb/project6/Library.java
+++ src/main/java/org/rcsb/project6/Library.java
@@ -15,7 +15,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.rcsb.hadoop.io.Demo;
 import org.rcsb.hadoop.io.HadoopToSimpleChainMapper;
-//import org.rcsb.hadoop.io.SimplePolymerChain;
+
 import org.rcsb.structuralAlignment.SuperPositionQCP;
 import org.rcsb.structuralSimilarity.GapFilter;
 import org.rcsb.structuralSimilarity.LengthFilter;
@@ -23,8 +23,8 @@ import org.rcsb.structuralSimilarity.LengthFilter;
 import scala.Tuple2;
 import scala.Tuple3;
 
-/**
- * 
+
+/* 
  * This class creates a library of unique fragments categorized by length.
  * Output PDBID as a string, length as a string, and the points as a point3d[].
  * 
@@ -39,66 +39,39 @@ public class Library
 	private static int length = 8;
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException
 	{
-		// arguments
-		String path = "/Users/grantsummers/Desktop/School/Internship/main/QuickStructureSearch/src/main/java/org/rcsb/project1/protein_chains_All_20150629_002251.seq";
-//		String path = "/Users/MellissaSummers/QuickStructureSearch/src/main/java/org/rcsb/project6/protein_chains_All_20150629_002251.seq";
-//		String path = "~/QuickStructureSearch/src/main/java/org/rcsb/project6/protein_chains_All_20150629_00251.seq";
 		
-//		String outputfilename = "output.txt";
-		 
-		// Spark setup
+		String path = "/Users/grantsummers/Desktop/School/Internship/main/QuickStructureSearch/src/main/java/org/rcsb/project1/protein_chains_All_20150629_002251.seq";
+
 		SparkConf conf = new SparkConf().setMaster("local[" + NUM_THREADS + "]")
 				.setAppName(Demo.class.getSimpleName())
 				.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		
-		// start time
-//		long start = System.nanoTime();
-		
-		// map sequence file to pairs (id, points)
 		List<Tuple2<String, Point3d[]>> chains = sc
 				.sequenceFile(path, Text.class, ArrayWritable.class,NUM_THREADS*NUM_TASKS_PER_THREAD)
-				.sample(false, 0.0003, 123)
-				.mapToPair(new HadoopToSimpleChainMapper()) // convert input to <pdbId.chainId, SimplePolymerChain> pairs
+				.sample(false, 0.003, 123)
+				.mapToPair(new HadoopToSimpleChainMapper()) 
 				.filter(t -> t._2.isProtein())
 				.mapToPair(t -> new Tuple2<String, Point3d[]>(t._1, t._2.getCoordinates()))
-				.filter(new GapFilter(0, 0)) // keep protein chains with gap size <= 0 and 0 gaps
-				.filter(new LengthFilter(50,500)) // keep protein chains with 50 - 500 residues
+				.filter(new GapFilter(0, 0)) 
+				.filter(new LengthFilter(50,500)) 
 				.collect();		
 		
-		// instantiations
-		// lib is the library (a list) of unique fragments
 		ArrayList<Tuple3<String, String, Point3d[]>> lib = new ArrayList<>();
-		// 2d arraylist of tuple2's that contains RMSDs between fragments in lib
-		// the second value in the tuple2 contains the horizontal index so that order can be
-		// found even after they are horizontally sorted (each row individually sorted)
-		ArrayList<ArrayList<Tuple2<Double, Integer>>> comparisons = new ArrayList<>();
-		ArrayList<Tuple2<Double, Integer>> templist = new ArrayList<>();
+		ArrayList<Double> templist = new ArrayList<>();
 		SuperPositionQCP qcp = new SuperPositionQCP(true);
-		// skiplist is a list of fragments in library to skip comparisons of
 		ArrayList<Integer> skiplist = new ArrayList<>();
-		// numnulls is a list of integers, where each integer represents the number
-		// of null values in comparisons in the corresponding column
-		ArrayList<Integer> numnulls = new ArrayList<Integer>();
-		// int length = 8;
 		int threshold = 1;
-		// integer for comparison's index
-		int ind;
-		// boolean for when to start adding to skiplist
-		boolean go = false;
-		// boolean for whether or not to add a fragment to the library
 		boolean bool = true;
-		int u;
+		int counter = 0;
+		ArrayList<String> title = new ArrayList<>();
+		ArrayList<Integer> freq = new ArrayList<>();
+		int number;
+
 		
-
-
-
-
-
 		for (Tuple2<String, Point3d[]> t: chains){
-			for (int star = 0; star < t._2.length-length; star++){								// for each fragment
-				
-				// code applied to (each) fragment with length 8 and start position star on chain t.
+			for (int star = 0; star < t._2.length-length; star++){
+				counter ++;
 				
 				Point3d[] fragment = new Point3d[length];
 				for (int i = 0; i < length; i++) {
@@ -106,106 +79,36 @@ public class Library
 				}
 				SuperPositionQCP.center(fragment);
 				
-				// fragment now has point3d[] = fragment, which is centered about centroid.
-				
 				Tuple3<String, String, Point3d[]> tup = 
 						new Tuple3<String, String, Point3d[]>
 							(t._1 + "." + star,
 							lengthy(fragment),
 							fragment);
-				
-				
+
 				if (lib.size() > 0) {
-					u = lib.size();
-					ind = lib.size() - 1;
-					
-					// loops through lib to check if tup is unique relative to lib's fragments
 					libloop: for (int i = 0; i < lib.size(); i++) {
 						if (lib.get(i)._2().equals(tup._2()) && !skiplist.contains(i)) {
 							qcp.set(lib.get(i)._3(), tup._3());
 							double q = qcp.getRmsd();
 							
-							Tuple2<Double, Integer> temptup = new Tuple2<Double, Integer>(q, ind);
-							ind--;
-							System.out.println("temptup - [q, ind]: [" + q + ", " + ind + "]");
-
-
-							
 							if (q < threshold) {
-								System.out.println("skipped due to similarity");
 								bool = false;
 								if(!templist.isEmpty()){
 									templist.clear();
 								}
-								break libloop;
-							}
-							templist.add(temptup);
-							
-							if (numnulls.size() >= i && numnulls.get(i) != null) { 
-								u -= numnulls.get(i);
-							}
-							
-							
-							
-							
-							 for (int a = 0; a < u; a++) {
-								 u--; // comparisons.size() is 1 less than lib.size() (theoretically)
-//								System.out.println("comparisons: " + comparisons.size());
-//								System.out.println("comparisons[a]: " + comparisons.get(a).size());
-//								System.out.println("(int) ((u-1-a)/2): " + ((int) ((u-a)/2)));
-//								System.out.println("comparisons[half]: " + comparisons.get((int) ((u-a)/2)).size());
-//								System.out.println("u: " + u);
-//								System.out.println("a: " + a);
-//								System.out.println("i: " + i);
-								if (comparisons.get(a).get(i) != null && comparisons.get((int) ((u-a)/2)).get(i) != null){
-									System.out.println("difference (q - comparisons): " + (q-comparisons.get(a).get(i)._1));
-									double difference = q - comparisons.get(a).get(i)._1;
-									double halfdiff = q - comparisons.get((int) ((u-a)/2)).get(i)._1;
-									if (go) {
-										System.out.println("we are go");
-										if (-difference > threshold) {
-											System.out.println("end go");
-											go = false;
-											break;
-										}
-										else if (!skiplist.contains(a)) {
-											skiplist.add(a);
-											System.out.println("Skiplist: " + a);
-										}
-									}
-									else {
-										if (difference <= threshold) {
-											System.out.println("start");
-											a -= 1;
-											while (difference <= threshold) {
-												a -= 1;
-											}
-											go = true;
-										}
-										if (halfdiff > threshold) {
-											System.out.println("1");
-											a = (int) ((u-a)/2);
-										}
-										else if (-halfdiff > threshold) {
-											System.out.println("2");
-											u = (int) ((u-a)/2);
-										}
-										else if (halfdiff == threshold) {
-											System.out.println("3 - start");
-											a = (int) ((u-a)/2) - 1;
-											while (difference <= threshold) {
-												a -= 1;
-											}
-											go = true;
-											
-										}
-									}
+								int key = searcher(title, lib.get(i)._1());
+								if (key != -1) {
+									number = freq.get(key) + 1;
+									freq.set(key, number);
 								}
 								else {
-									System.out.println("derp: failed triangle inequality (prolly a sort - insertion - problem)");
+									System.out.println("failure");
+									title.add(tup._1());
+									freq.add(1);
 								}
+								break libloop;
 							}
-							System.out.println("I'm' outta the loop");
+							templist.add(q);
 						}
 						else {
 							templist.add(null);
@@ -213,41 +116,10 @@ public class Library
 					}
 					skiplist.clear();
 
-					System.out.println("templist: " + templist.size());
+
 					if (bool == true) {
-						vert: for (int vert = 0; vert < lib.size(); vert++) {
-							hor: for (int hor = vert; hor < lib.size(); hor++) {
-								if (comparisons != null && !comparisons.isEmpty()) {
-									if (templist.get(vert) != null) {
-										if (templist.get(vert)._1 <= comparisons.get(hor).get(vert)._1 || hor == lib.size() - 1) {
-											comparisons.get(hor).add(templist.get(vert));
-											break hor;
-										}
-									}
-									else {
-										//templist(vert) is null
-										System.out.println("comparisons: " + comparisons.size());
-										System.out.println("lib-1: " + (lib.size()-1));
-										comparisons.get(lib.size()-1).add(vert, null);
-										if (numnulls != null && numnulls.size() > vert) {
-											numnulls.set(vert, numnulls.get(vert) + 1);
-										}
-										else if (numnulls.size() < vert - 1) {
-//											while (numnulls.size() < vert - 2) {
-//												numnulls.add(null);
-//											}
-											numnulls.add(1);
-										}
-									}
-								}
-								else {
-									//comparisons is completely null
-									comparisons = new ArrayList<ArrayList<Tuple2<Double, Integer>>>();
-									comparisons.add(templist);
-									break vert;
-								}
-							}
-						}
+						title.add(tup._1());
+						freq.add(1);
 						templist.clear();
 						lib.add(tup);
 					}
@@ -257,38 +129,43 @@ public class Library
 				}
 				else {
 					lib.add(tup);
+					freq.add(1);
+					title.add(tup._1());
 				}
 			}
 		}
 		sc.close();
 
-
-
-
-		PrintWriter writer = new PrintWriter("library.tsv", "UTF-8");
-		for (Tuple3<String, String, Point3d[]> l: lib) {
-			writer.print("PDBID.ChainID\tlength");
-			for (int i=0; i<length; i++) {
-				writer.print("\tPoint " + i);
-			}
-			writer.println();
-			writer.print(l._1() + "\t [" + l._2() + "]");
-			for (Point3d p: l._3()) {
+		PrintWriter writer = new PrintWriter("library.txt", "UTF-8");
+		writer.print("PDBID.ChainID\tlength");
+		for (int i=1; i<=length; i++) {
+			writer.print("\tPoint " + i);
+		}
+		writer.print("\tFrequency");
+		writer.println();
+		for (int i=0; i < lib.size(); i++) {
+			writer.print(lib.get(i)._1() + "\t [" + lib.get(i)._2() + "]");
+			for (Point3d p: lib.get(i)._3()) {
 				writer.print("\t" + p);
 			}
+			writer.print("\t" + freq.get(i));
 			writer.println();
 		}
 		writer.close();
 		System.out.println("library size: " + lib.size());
-		
-		//System.out.println("Time: " + (System.nanoTime() - start)/1E9 + " sec.");
+		System.out.println("initial size: " + counter);
+	}
+	
+	public static Integer searcher (ArrayList<String> a, String b) {
+		for (int s = 0; s < a.size(); s++) {
+			if (a.get(s).equals(b)) {
+				return s;
+			}
+		}
+		return -1;
 	}
 
-
-
-
-	// FIND OPTIMAL LENGTH
-	public static String lengthy(Point3d[] p){
+	public static String lengthy (Point3d[] p) {
 		int round = 2;
 		double i = Math.abs(p[p.length-1].distance(p[0]));
 		i /= round;
