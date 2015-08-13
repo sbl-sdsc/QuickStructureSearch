2df977463fd66981b9a4b1ee2af544be7cc1c301
Justin Dong Li <justindli@gmail.com>
2015-08-13 15:32:11 -0700

    Added a SmithWaterman class and an implementation for triangle inequality.

--- src/main/java/org/rcsb/project5/StructuralClusterCreator.java
+++ src/main/java/org/rcsb/project5/StructuralClusterCreator.java
@@ -60,6 +60,154 @@ public class StructuralClusterCreator {
 		}
 		return structuralCluster;
 	}
+	
+	/**
+	 * Returns a list of structural clusters given a sequence cluster and a maximum RMSD value for each cluster utilizing the triangle inequality
+	 * @param allClusters
+	 * @param maxRmsd
+	 * @return
+	 */
+	public List<List<Tuple2<String, SimplePolymerChain>>> createStructuralClusterTI(List<Tuple2<String, SimplePolymerChain>> allClusters, double maxRmsd) {
+		boolean[][] thresPairs = new boolean[allClusters.size()][allClusters.size()];
+		/* Initialization */
+		boolean[] done = new boolean[allClusters.size()];
+		for(int i = 0; i < done.length; i++) {
+			done[i] = false;
+		}
+		/*for(int i = 0; i < allClusters.size(); i++) {
+			if(allClusters.get(i)._1.equals("2IOK.A") || allClusters.get(i)._1.equals("3ERT.A")) {
+				System.out.println(allClusters.get(i)._1 + ": " + allClusters.get(i)._2.getSequence());
+			}
+		}
+		System.exit(-1);*/
+		/* Iterations */
+		for(int i1 = 0; i1 < done.length; i1++) {
+			int n_cmp = 0;
+			int k = 0;
+			int[] index = new int[done.length - i1];
+			double[] rmsd = new double[index.length];
+
+			for(int i2 = i1 + 1; i2 < done.length; i2++) {
+				if(!done[i2]) {
+					index[k] = i2;
+					rmsd[k] = RMSD(allClusters.get(i1), allClusters.get(i2));
+					n_cmp += 1;
+					if(rmsd[k] <= maxRmsd) {
+						thresPairs[i1][i2] = true;
+						thresPairs[i2][i1] = true;
+					}
+					k += 1;
+				}
+			}
+
+			done[i1] = true;
+			int n_left = k;
+			List<Tuple2<Integer, Double>> index_rmsd = new ArrayList<Tuple2<Integer, Double>>(k);
+			for(int count = 0; count < k; count++) {
+				index_rmsd.add(new Tuple2<Integer, Double>(index[count], rmsd[count]));
+			}
+
+			if(index_rmsd.size() > 0) {
+				quickSort(index_rmsd, 0, index_rmsd.size() - 1);
+			}
+
+			double ratio = 0;
+			for(int j = 0; ((n_cmp == 0 || ((double)(j+1)/n_cmp) > ratio)) && j<n_left; j++) {
+				ratio = ((j+1)/n_cmp);
+
+				for(int l=j+1; l<n_left; l++) {
+					if ((index_rmsd.get(l)._2 - index_rmsd.get(j)._2) <= maxRmsd) {
+						double tempRmsd = RMSD(allClusters.get(index_rmsd.get(l)._1), allClusters.get(index_rmsd.get(j)._1));
+						n_cmp += 1;
+						if(tempRmsd <= maxRmsd) {
+							thresPairs[index_rmsd.get(j)._1][index_rmsd.get(l)._1] = true;
+							thresPairs[index_rmsd.get(l)._1][index_rmsd.get(j)._1] = true;
+						}
+					}
+				}
+				done[index_rmsd.get(j)._1] = true;
+			}
+		}
+		/*for(int i = 0; i < thresPairs.length; i++) {
+			for(int j = 0; j < thresPairs[i].length; j++) {
+				if(thresPairs[i][j]) {
+					System.out.print("1 ");
+				} else {
+					System.out.print("0 ");
+				}
+			}
+			System.out.println();
+		}*/
+		ArrayList<Tuple2<List<Tuple2<String, SimplePolymerChain>>, List<Integer>>> tempStructuralCluster = new ArrayList<Tuple2<List<Tuple2<String, SimplePolymerChain>>, List<Integer>>> ();
+		structuralCluster = new ArrayList<List<Tuple2<String, SimplePolymerChain>>> ();
+		for(int a = 0; a < allClusters.size(); a++) {
+			boolean unique = true;
+			for(Tuple2<List<Tuple2<String, SimplePolymerChain>>, List<Integer>> cluster: tempStructuralCluster) {
+				boolean inCluster = true;
+				for(int b = 0; b < cluster._1.size(); b++) {
+					if(!thresPairs[cluster._2.get(b)][a])
+						inCluster = false;
+				}
+				if(inCluster == true) {
+					cluster._1.add(allClusters.get(a));
+					cluster._2.add(a);
+					unique = false;
+					break;
+				}
+			}
+			if(unique == true) {
+				Tuple2<List<Tuple2<String, SimplePolymerChain>>, List<Integer>> newCluster = new Tuple2<List<Tuple2<String, SimplePolymerChain>>, List<Integer>>(new ArrayList<Tuple2<String, SimplePolymerChain>>(), new ArrayList<Integer>());
+				newCluster._1.add(allClusters.get(a));
+				newCluster._2.add(a);
+				tempStructuralCluster.add(newCluster);
+			}
+		}
+		for(Tuple2<List<Tuple2<String, SimplePolymerChain>>, List<Integer>> cluster: tempStructuralCluster) {
+			structuralCluster.add(cluster._1);
+		}
+		return structuralCluster;
+	}
+
+	/**
+	 * Returns the cRMSD value of the two chains over the longest common sequence between them
+	 * @param tuple
+	 * @param tuple2
+	 * @return
+	 */
+	private double RMSD(Tuple2<String, SimplePolymerChain> tuple, Tuple2<String, SimplePolymerChain> tuple2) {
+		List<Integer> startEnd = lcs.longestCommonSubstring(tuple._2.getSequence(), tuple2._2.getSequence());
+		return getcRmsd(tuple, tuple2, startEnd.get(0), startEnd.get(1), startEnd.get(2), startEnd.get(3));
+	}
+
+	void quickSort (List<Tuple2<Integer, Double>> list, int first, int last)
+	{
+		int g = first, h = last;
+
+		int midIndex = (first + last) / 2;
+		double dividingValue = list.get(midIndex)._2;
+		do
+		{
+			while (list.get(g)._2 < dividingValue)
+				g++;
+			while (list.get(h)._2 > dividingValue)
+				h--;
+			if (g <= h)
+			{
+				//swap(list[g], list[h]);
+				Tuple2<Integer, Double> temp = list.get(g);
+				list.set(g, list.get(h));
+				list.set(h, temp);
+				g++;
+				h--;
+			}
+		}
+		while (g < h);
+
+		if (h > first)
+			quickSort (list,first,h);
+		if (g < last)
+			quickSort (list,g,last);
+	}
 
 	/**
 	 * Returns the cRMSD value of a specific portion of the Point3D arrays of the two chains given the starting and ending positions of the specific portions
