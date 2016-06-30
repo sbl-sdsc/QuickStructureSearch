7d01b3973e0a4e6c14c072c59c19f646279e5018
Emilia Copic <ottolucia@aol.com>
2016-06-30 11:35:43 -0700

    Tested a few more methods

--- src/main/java/org/rcsb/projectec/MeetMinIndexMapperP3.java
+++ src/main/java/org/rcsb/projectec/MeetMinIndexMapperP3.java
@@ -47,7 +47,11 @@ public class MeetMinIndexMapperP3 implements AlignmentAlgorithmInterface {
 	public Tuple2<String, Float> call(Tuple2<String, String> tuple) throws Exception {
 		SequenceFeatureInterface<Integer> v1 = (SequenceFeatureInterface<Integer>) this.sequences.getValue().get(tuple._1);
 		SequenceFeatureInterface<Integer> v2 = (SequenceFeatureInterface<Integer>) this.sequences.getValue().get(tuple._2);
-
+		
+		if (v1 == null || v2 == null) {
+			return null;
+		}
+		
 		String key = tuple._1 + "," + tuple._2;
 		float value = meetMinIndex(v1, v2);
 	
