681685a601e518e43b72cfa81bd0cf04999e8756
Emilia Copic <ottolucia@aol.com>
2016-07-01 13:37:15 -0700

    Added FlatMapToFragments and updated ArchLibGenerator

--- src/main/java/org/rcsb/projectec/ArchLibGenerator.java
+++ src/main/java/org/rcsb/projectec/ArchLibGenerator.java
@@ -2,11 +2,15 @@ package org.rcsb.projectec;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 
+import javax.vecmath.Point3d;
+
 import org.apache.hadoop.io.Text;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.broadcast.Broadcast;
 import org.rcsb.mmtf.spark.utils.SparkUtils;
@@ -24,59 +28,46 @@ import scala.Tuple2;
  *
  */
 public class ArchLibGenerator {
-	private int fragLenth = 6;
 	public static void main(String[] args) {
 		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+		
 		System.out.println("Broadcast Method");
 		
 		String chainFile = args[0];
 		System.out.println("Chain file    : " + chainFile);
 		
-		String libraryFile = args[1] + File.separatorChar + "ArchetypeLibrary_" + timeStamp + ".csv";
-	    System.out.println("Benchmark file: " + libraryFile);
-		
 	    long start = System.nanoTime();
 
-		double fraction = 0.0002;
-		int randomSeed = 1;
-	    int combinations = run(chainFile, libraryFile, fraction, randomSeed);
-	    
+	//	double fraction = 0.0002;
+	//	int randomSeed = 1;
+	 //   int combinations = run(chainFile, benchmarkFile, fraction, randomSeed);
+	    getAllFragments(chainFile);
 	    long end = System.nanoTime();
          
 	    double time = (end-start)/1E9;
 	    System.out.println("Time          : " + (end-start)/1E9 + " seconds"); 
-	    System.out.println("Chain pairs   : " + combinations);
-	    System.out.println("Throughput    : " + combinations/time + " pairs/seconds");
+	//    System.out.println("Chain pairs   : " + combinations);
+	  //  System.out.println("Throughput    : " + combinations/time + " pairs/seconds");
 	}
 
-	private static int run(String chainFile, String benchmarkFile,
-			double fraction, int randomSeed) {
+	private static JavaRDD<Point3d[]> getAllFragments(String chainFile){
 		JavaSparkContext sc = SparkUtils.getSparkContext();
 	    
-	    List<Tuple2<String, WritableSegment>> segments = sc
+	    JavaPairRDD<String, Point3d[]> mapToPair = sc
 	    		.sequenceFile(chainFile, Text.class, WritableSegment.class) // read file with chains
 	    		.mapToPair(t -> new Tuple2<String, WritableSegment> (new String(t._1.toString()), new WritableSegment(t._2)) ) // make a copy of the data
-	    		.sample(false, fraction, randomSeed) // take a sample
-	    		.collect();
-	    
-	    int numPairs = (segments.size()*(segments.size()-1))/2;
-	    
-	    // broadcast segment data to all nodes
-	    final Broadcast<List<Tuple2<String,WritableSegment>>> data = sc.broadcast(segments); 
+	    		.mapToPair(t -> new Tuple2<String, Point3d[]>(t._1, t._2.getCoordinates()));
+	
+	    JavaRDD<Point3d[]>fragments = mapToPair.map(t->t._2()).flatMap(new FlatMapToFragments(6));
+	    fragments.foreach(t-> System.out.println(Arrays.toString(t)));
 
-	    // get indices for all unique pairs given the number of segments
-	    JavaPairRDD<Integer, Integer> pairs = SparkUtils.getComparisonMatrix(segments.size()); 
 	    
-	    // run structural alignment and save results in .csv file
-
-	    pairs
-	    	.repartition(8)
-	    	.map(new StructuralAlignmentMapper(data))
-	    	.saveAsTextFile(benchmarkFile);
-	 
+	  
 	    sc.stop();
 	    sc.close();
 	    
-		return numPairs;
+		return fragments;
 	}
+	
+	
 }
